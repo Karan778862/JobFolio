2df2e012452e4af54582d76e42ce55820db93e4b
karan <kkpkaran22@gmail.com>
2025-01-21 01:02:40 +0530

    url change

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 
 const _dirname = path.resolve();
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://jobfolio-kdgz.onrender.com',
     credentials:true
 };
 app.use(cors(corsOptions));
